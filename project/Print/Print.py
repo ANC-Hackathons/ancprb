83a49fbe4297461facab2415bbfa832587723c66
Jason Nedell <jason@nedell.com>
2015-09-06 04:35:45 -0600

    Try to implement Grepl

--- project/Print/Print.py
+++ project/Print/Print.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 
+from . import printrun
+
+class Print:
+  def __init__(self, port, baud) :
+    self.port    = port
+    self.baud    = baud
+    self.printer = printrun.printcore(self.port, self.baud)
+
+  def send_gcode(self, gcode) :
+    self.printer.send()
