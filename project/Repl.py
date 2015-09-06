52c9d3f2d434d07dda0c7945419b271ea6a4d0b8
Jason Nedell <jason@nedell.com>
2015-09-06 04:03:13 -0600

    Move packages to package directory. Shrug.

--- project/Repl.py
+++ project/Repl.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 
+from Print.printrun.printcore import printcore
+
+if __name__ == "__main__":
+	printer = printcore('/dev/tty.usbmodemfa131', 115200)
+
+	while (1) :
+		command = raw_input("cmd: ")
+		printer.send(command)
