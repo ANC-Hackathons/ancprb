833153dcda20ce49a01d4e16ebd7b2995590f307
Michael Ferenc <Michael@Babylon.local>
2015-09-06 02:22:55 -0600

    Some game code changes

--- flightpath/game_loop.py
+++ flightpath/game_loop.py
@@ -6,11 +6,11 @@ import game_objects
 import lulzbot_interface
 
 # Game Constants
-time_step = 1.0 # seconds
+time_step = 0.1 # seconds
 max_time = 10.0
 
 # Ship Constants
-ship_speed = 1.0 # mm/sec
+ship_speed = 0.5 # mm/time
 init_position = np.array([22,71,0],np.float32)
 init_direction = np.radians(0)
 init_rate = np.radians(10)
@@ -18,20 +18,23 @@ ship_x_dim = 1.0
 ship_y_dim = 1.0
 
 # Map Properties
-map_file = 'map_file.dat'
+# map_file = 'map_file.dat'
+map_file = 'empty_map.dat'
 num_cells = [128,128]
 map_dimensions = [145.0,145.0] # physical map size in mm
 
 # Lulzbot properties
 lulz_x = [15,150]
 lulz_y = [15,140]
-lulz_z = [100,150]
-offset = np.array([5,5,130],np.float32) # vector from origin of lulz coordinate system to map coordinate system
+lulz_z = [40,150]
+offset = np.array([5,5,60],np.float32) # vector from origin of lulz coordinate system to map coordinate system
+flight_feedrate = (ship_speed / time_step) * 60.0 # mm/min
+fast_feedrate = 1500.0 # mm/min
 
 # Instantiate game objects and game variables
 flying_toaster = game_objects.Ship(init_position,ship_speed,init_direction,init_rate,ship_x_dim,ship_y_dim)
 the_canyon = game_objects.Map(map_file,num_cells,map_dimensions)
-the_lulz = lulzbot_interface.Lulzbot(lulz_x,lulz_y,lulz_z,offset)
+the_lulz = lulzbot_interface.Lulzbot(lulz_x,lulz_y,lulz_z,offset,flight_feedrate,fast_feedrate)
 
 game_time = 0.0
 collision_status = 0
@@ -43,14 +46,14 @@ printer.send(the_lulz.zero())
 time.sleep(30.0)
 
 # Go to starting position
-command = the_lulz.send_g_code(flying_toaster.position)
+command = the_lulz.send_g_code(flying_toaster.position,"fast")
 print command
 printer.send(command)
-time.sleep(10.0)
+time.sleep(25.0)
 
 while(game_time < max_time and collision_status == 0):
  	# Gather data from Pebble
- 	rotate = np.radians(0.0)
+ 	rotate = np.radians(5.0)
 
  	# Update game objects
  	flying_toaster.turn(rotate)
@@ -64,7 +67,7 @@ while(game_time < max_time and collision_status == 0):
  	# print "(%d,%d): %d" % (coordinates[0],coordinates[1],cell_state)
 
  	# Send data to Lulzbot
- 	command = the_lulz.send_g_code(flying_toaster.position)
+ 	command = the_lulz.send_g_code(flying_toaster.position,"slow")
  	print command
  	printer.send(command)
 
--- flightpath/lulzbot_interface.py
+++ flightpath/lulzbot_interface.py
@@ -1,18 +1,24 @@
 import numpy as np
 
 class Lulzbot:
-	def __init__(self,x_lims,y_lims,z_lims,offset):
+	def __init__(self,x_lims,y_lims,z_lims,offset,slow_feedrate,fast_feedrate):
 		self.x_lims = x_lims
 		self.y_lims = y_lims
 		self.z_lims = z_lims
 		self.offset = offset
+		self.slow_feedrate = slow_feedrate
+		self.fast_feedrate = fast_feedrate
 
 	def zero(self):
 		return "G28"
 
-	def send_g_code(self,pos):
+	def send_g_code(self,pos,rate):
+		cur_feedrate = self.slow_feedrate
+		if rate == "fast":
+			cur_feedrate = self.fast_feedrate
+
 		position = pos + self.offset
 		if (position[0] < self.x_lims[0] or position[0] > self.x_lims[1] or position[1] < self.y_lims[0] or position[1] > self.y_lims[1] or position[2] < self.z_lims[0] or position[2] > self.z_lims[1]):
 			return "error!"
 		else:
-			return "G1 X%d Y%d Z%d" % (position[0],position[1],position[2])
+			return "G1 X%.3f Y%.3f Z%.3f F%.3f" % (position[0],position[1],position[2],cur_feedrate)
